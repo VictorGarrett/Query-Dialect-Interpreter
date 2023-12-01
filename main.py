aa6d0900598a560216cccd81634cbb017ada56ea
jpmaciels <joaopiams@gmail.com>
2023-12-01 10:29:39 -0300

    Start of interpeter

--- main.py
+++ main.py
@@ -1,9 +1,190 @@
 import csv_importer
 from table import join_tables
-from mysql_importer import import_mysql_to_database
+import mysql_importer as mysql_imp
 import file_handler
 
-import_mysql_to_database("root", "pwd", "employees", ["departments", "employees"], "test_emp")
-csv_importer.import_dir_csv_to_database(".", "test_csv")
+
+# import_mysql_to_database("root", "NuncaFoiMole.123",
+#                           "employees", ["departments", "employees"], "test_emp")
+# csv_importer.import_dir_csv_to_database(".", "test_csv")
 
 
+# treat the command spliting the statements
+def parser(query: str):
+    global commands
+
+    commands = {
+        "select": None,
+        "update": None,
+        "set": None,
+        "insert": None,
+        "delete": None,
+        "into": None,
+        "values": None,
+        "from": None,
+        "join": None,
+        "on": None,
+        "using": None,
+        "where": None,
+        "and": None,
+        "or": None,
+        "order by": None,
+    }
+    try:
+        query = query.replace(", ", ",")
+        query = query.replace(" ,", ",")
+        query = query.replace(" =", "=")
+        query = query.replace("= ", "=")
+        query = query.replace(" >=", ">=")
+        query = query.replace(">= ", ">=")
+        query = query.replace(" <=", "<=")
+        query = query.replace("<= ", "<=")
+        query = query.replace(" >", ">")
+        query = query.replace("> ", ">")
+        query = query.replace(" <", "<")
+        query = query.replace("< ", "<")
+        query = query.replace("order by", "orderby")
+
+        # splits sql command using space as separator
+        query_list = query.split()
+
+        # extract table in query
+        if "from" in query_list:
+            i = query_list.index("from")
+            table = query_list[i + 1]
+            commands["from"] = table, i
+            # extract join argument
+            if "join" in query_list:
+                i = query_list.index("join")
+                join_table = query_list[i + 1]
+                commands["join"] = join_table, i
+                if join_table in commands:
+                    print("Error : Wrong argument near {}".format(join_table))
+                    return False
+                if "on" in query_list:
+                    i = query_list.index("on")
+                    join_column = query_list[i + 1]
+                    commands["on"] = join_column, i
+                    if join_column in commands:
+                        print("Error : Wrong argument near {}".format(join_column))
+                        return False
+                elif "using" in query_list:
+                    i = query_list.index("using")
+                    join_column = query_list[i + 1]
+                    commands["using"] = join_column, i
+                    if join_column in commands:
+                        print("Error : Wrong arguments near {}".format(join_column))
+                        return False
+                else:
+                    print("Error : Wrong argument near  {}".format(join_table))
+                    return False
+        elif "update" in query_list:
+            i = query_list.index("update")
+            table = query_list[i + 1]
+            commands["update"] = table, i
+        elif "into" in query_list:
+            i = query_list.index("into")
+            table = query_list[i + 1]
+            commands["into"] = table, i
+        else:
+            print("Error : Wrong argument near {}".format(table))
+            return 0
+
+        # verification if arguments is part of commands
+        if table in commands:
+            print("Error : wrong argument {}".format(table))
+            return 0
+
+        if "select" in query_list:
+            i = query_list.index("select")
+            columns = query_list[i + 1]
+            commands["select"] = columns, i
+        elif "update" in query_list:
+            i = query_list.index("set")
+            set = query_list[i + 1]
+            commands["set"] = set, i
+        elif "insert" in query_list:
+            i = query_list.index("values")
+            values = query_list[i + 1]
+            commands["values"] = values, i
+        elif "delete" in query_list:
+            i = query_list.index("from")
+            delete = " "
+            commands["delete"] = delete, i
+        else:
+            print("Error : unexpected")
+            return False
+
+        # catch where statement
+        if "where" in query_list:
+            i = query_list.index("where")
+            clause = query_list[i + 1]
+            commands["where"] = clause, i
+
+        # catch and or statement
+        if "and" in query_list:
+            i = query_list.index("and")
+            clause = query_list[i + 1]
+            commands["and"] = clause, i
+        elif "or" in query_list:
+            i = query_list.index("or")
+            clause = query_list[i + 1]
+            commands["or"] = clause, i
+
+        # catch order by
+        if "orderby" in query_list:
+            i = query_list.index("orderby")
+            clause = query_list[i + 1]
+            commands["order by"] = clause, i
+
+        """ # if(is_query_valid()):
+        data = _from()
+
+        if tuple_value(commands["select"]):
+            _select(data)
+        elif tuple_value(commands["delete"]):
+            _delete(data)
+        elif tuple_value(commands["into"]):
+            _insert(data)
+        elif tuple_value(commands["update"]):
+            _update(data) """
+
+    except:
+        print("Error : Invalid query")
+        return False
+
+
+def importer():
+    option = None
+    while not (option == "mysql" or option == "csv"):
+        print(
+            "mysql - Importar do banco MySQL\ncsv   - Importar um arquivo .CSV\n(mysql/csv)")
+        option = input(">> ")
+    if option == "mysql":
+        mysql_imp.importer()
+    # elif option == "csv":
+        # csv_import.csv_import()
+    return
+
+
+def main():
+    # takes query from user
+    answer = None
+    while not (answer == "i" or answer == "c" or answer == "s"):
+        print("i - Importar \nc - Consualtar(query) \ns - sair?\n(i/c/s)")
+        answer = input(">> ")
+    if answer == "i":
+        print("Importar!\n")
+        importer()
+    elif answer == "c":
+        print("Consultar!\n")
+        # query()
+    elif answer == "s":
+        return False
+
+    return True
+
+
+if __name__ == "__main__":
+    while main():
+        continue
--- mysql_importer.py
+++ mysql_importer.py
@@ -2,40 +2,98 @@ import mysql.connector as connector
 from table import Table
 import file_handler
 
+user_global = 'root'
+password_global = 'NuncaFoiMole.123'
+database_global = 'employees'
+host_global = '127.0.0.1'
+destination = 'CSV_Database'
 
 
-def import_mysql_to_database(user: str, pwd: str, mysql_database: str, tables: list, to_database: str):
-    cnx = connector.connect(user=user, password=pwd,
-                                    host='127.0.0.1',
-                                    database=mysql_database)
+def mysql_connector():
+
+    cnx_params = {
+        'host': host_global,
+        'user': user_global,
+        'password': password_global,
+        'database': selected_database_global,
+    }
+
+    try:
+        db_connection = connector.connect(**cnx_params)
+    except:
+        print("error : Schema not found")
+        return False
+
+    print('\nConectado!\n')
+    return db_connection
+
+
+def list_mysql_schemas():
+    cnx = connector.connect(user=user_global, password=password_global,
+                            host=host_global, database=database_global)
+    cursor = cnx.cursor()
+    cursor.execute("SHOW DATABASES;")
+    print("\nEsquemas no servidor MySQL: ")
+    for (databases) in cursor:
+        print('* '+databases[0])
+
+
+def list_mysql_tables(cursor):
+    print("Tabelas do esquema {}:".format(selected_database_global))
+    cursor.execute("SHOW TABLES;")
+    for row in cursor:
+        for key in row:
+            print('* ' + row[key] .strip("'"))
+
+
+def importer():
+
+    global selected_database_global
+
+    list_mysql_schemas()
+    cnx = None
+    while not cnx:
+        print("Digite nome do esquema a selecionar: ")
+        selected_database_global = input('>> ')
+        cnx = mysql_connector()
+
+    cursor = cnx.cursor(dictionary=True, buffered=True)
+
+    list_mysql_tables(cursor)
+    print("Digite nome da(s) tabela(s) a selecionar (Utilizar notação ): ")
+    tables = input('>> ')
+
+    cnx = mysql_connector()
 
     if cnx and cnx.is_connected():
 
         with cnx.cursor() as cursor:
 
             for table_name in tables:
-                #cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables where TABLE_SCHEMA = \"employees\" AND TABLE_TYPE = \"BASE TABLE\"")
-                cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.columns where TABLE_SCHEMA = \"" + mysql_database + "\" AND TABLE_NAME = \"" + table_name +"\"")
+                # cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables where TABLE_SCHEMA = \"employees\" AND TABLE_TYPE = \"BASE TABLE\"")
+                cursor.execute(
+                    "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.columns where TABLE_SCHEMA = \"" + database_global + "\" AND TABLE_NAME = \"" + table_name +"\"")
 
-                #print("rows")
+                print("rows")
                 rows = cursor.fetchall()
-                #print(rows)
-                #print("for")
+                print(rows)
+                print("for")
                 field_names = []
                 for row in rows:
                     field_names.append(row[0])
                     print(row)
 
-                #print(field_names)
+                print(field_names)
 
                 field_names_str = ""
                 for field in field_names:
                     field_names_str = field_names_str + field + ', '
 
                 field_names_str = field_names_str[:-2]
-                #print(field_names_str)
+                print(field_names_str)
 
-                cursor.execute("SELECT " + field_names_str + " FROM " + table_name)
+                cursor.execute("SELECT " + field_names_str +
+                               " FROM " + table_name)
 
                 rows = cursor.fetchall()
 
@@ -47,10 +105,10 @@ def import_mysql_to_database(user: str, pwd: str, mysql_database: str, tables: l
                         new_row[field_names[i]] = row[i]
                     new_table.add_row(new_row)
 
-                #print("newtable")
-                #print(new_table.data)                
-                #importer.export_table(table_name + ".csv", new_table)
-                file_handler.save_table_to_database(new_table, to_database)
+                print("newtable")
+                print(new_table.data)
+                # importer.export_table(table_name + ".csv", new_table)
+                file_handler.save_table_to_database(new_table, destination)
 
         cnx.close()
 
